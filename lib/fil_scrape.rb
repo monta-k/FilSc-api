78d6a30edc5dcf524aa035501c901c7264010586
Kobayashi Shunta <monta.k0523@gmail.com>
2019-08-30 17:04:10 +0900

    user parallel

--- lib/fil_scrape.rb
+++ lib/fil_scrape.rb
@@ -1,30 +1,31 @@
 require 'open-uri'
 require 'nokogiri'
+require 'parallel'
 
  class FilScrape
   def self.scrape(userId)
     base_url = 'https://filmarks.com/'
 
-     charset = nil
+    charset = nil
     html = open(base_url + "users/#{userId}/clips") do |f|
       charset = f.charset
       f.read
     end
 
-     doc = Nokogiri::HTML.parse(html, nil, charset)
+    doc = Nokogiri::HTML.parse(html, nil, charset)
 
-     title = doc.title
+    title = doc.title
     page = doc.xpath("//ul[@class='c-pagination__list']//li").count
 
-     movie = []
+    movie = []
 
-     page.times do |i|
+    page.times do |i|
       html2 = open(base_url + "users/monta.k/clips?page=#{i + 1}") do |f|
         charset = f.charset
         f.read
       end
       doc2 = Nokogiri::HTML.parse(html2, nil, charset)
-      movie_a = doc2.xpath("//h3[@class='c-movie-item__title']//a").map do |item|
+      movie_a = Parallel.map(doc2.xpath("//h3[@class='c-movie-item__title']//a")) do |item|
         html3 = open(base_url + item[:href]) do |f|
           charset = f.charset
           f.read
@@ -38,6 +39,7 @@ require 'nokogiri'
         [movie_title, movie_length, movie_score, movie_img, movie_link]
       end
       movie.concat(movie_a)
+      puts movie.count
     end
 
      return { title: title, movie: movie}
