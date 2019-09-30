fabb397629d14b3c1234e2af8e634c0d3e8c524e
Kobayashi Shunta <monta.k0523@gmail.com>
2019-09-30 18:37:40 +0900

    fix

--- app/controllers/api/v1/popular_movies_controller.rb
+++ app/controllers/api/v1/popular_movies_controller.rb
@@ -2,10 +2,7 @@ module Api
   module V1
     class PopularMoviesController < ApplicationController
       def show
-        popular_movies = PopularMovie.order('RANDOM()').limit(20)
-        @movies = popular_movies.map do |m|
-          TmdbMovie.find(m.tmdb_movie_id)
-        end
+        @movies = PopularMovie.order('RANDOM()').limit(20)
         render 'show', formats: 'json', handlers: 'jbuilder'
       end
     end
--- app/models/popular_movie.rb
+++ app/models/popular_movie.rb
@@ -1,2 +1,3 @@
 class PopularMovie < ApplicationRecord
+  belongs_to :tmdb_movie
 end
